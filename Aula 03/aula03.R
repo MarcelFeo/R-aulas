81e26a3d9b200e80f7fc91a027f87b7516568e86
MarcelFeo <marcelfeo29@gmail.com>
2024-08-14 17:44:09 -0300

    analisando o numero de mortos

--- Aula 03/aula03.R
+++ Aula 03/aula03.R
@@ -1,1 +1,5 @@
-# Aula 03 - 
+# Aula 03 - Analise de dados
+
+# dados do arquivo titanic.txt, primeira linha são nomes das colunas e os valores são separados por vírguça
+dados <- read.table(file = "Aula 03/titanic.txt", header = TRUE, sep = ",")
+
